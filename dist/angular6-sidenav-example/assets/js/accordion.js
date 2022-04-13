83c2cb43efec6ec7d0e19f944716bf5891ceaabb
kainosinnovative@gmail.com <KainosGithub1>
2022-04-13 19:01:10 +0530

    ui changes

---
+++ dist/angular6-sidenav-example/assets/js/accordion.js
@@ -0,0 +1,12 @@
+
+    $('.accordion').each(function () {
+    var $accordian = $(this);
+    $accordian.find('.accordion-head').on('click', function () {
+        $(this).removeClass('open').addClass('close');
+        $accordian.find('.accordion-body').slideUp();
+        if (!$(this).next().is(':visible')) {
+            $(this).removeClass('close').addClass('open');
+            $(this).next().slideDown();
+        }
+    });
+});
